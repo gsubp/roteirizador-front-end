60a8857cbc70e128feea2ccf4750bed981ab3ee7
Guilherme Pereira <guilherme.h12@gmail.com>
2021-10-07 18:06:45 -0300

    add MAPBOX  and MAPBOX Directions to project & remove SideBar

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,29 +1,62 @@
-import React, { useEffect, useState } from "react";
-import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps"
+import React, { useRef, useEffect, useState } from 'react';
+import mapboxgl from 'mapbox-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
+import '@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions.css';
+import MapboxDirections from "@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions";
+import MapboxLanguage from '@mapbox/mapbox-gl-language';
 
 
-export default function Map() { 
-    const location = {
-        lat: 0,
-        lng: 0
-    }
-    navigator.geolocation.getCurrentPosition((position) => {
-        location.lat = position.coords.latitude;
-        location.lng = position.coords.longitude;
+mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_API_KEY;
+
+export default function Map() {
+    const mapContainer = useRef(null);
+    const map = useRef(null);
+    const directions = useRef(null);
+    const language = useRef(null);
+    const [lng, setLng] = useState(-70.9);
+    const [lat, setLat] = useState(42.35);
+    const [zoom, setZoom] = useState(9);
+    
+    useEffect(() => {
+        if (map.current) return; // initialize map only once
+            map.current = new mapboxgl.Map({
+                container: mapContainer.current,
+                style: 'mapbox://styles/mapbox/streets-v11',
+                center: [lng, lat],
+                zoom: zoom,
+
+            });
+        directions.current = new MapboxDirections({
+            accessToken: mapboxgl.accessToken,
+            unit: 'metric',
+            profile: 'mapbox/driving',
+            language: 'pt-br'
+
+        });
+        directions.current.on('route', (e) => {
+            alert("Fire")
+            console.log(e)
+            console.log(directions.current.getOrigin())
+            console.log(directions.current.getDestination())
+        }); 
+        language.current = new MapboxLanguage({
+            defaultLanguage: 'pt-br',
+        });
+        map.current.addControl(directions.current, 'top-left');
     });
 
-    const MapComponent = withScriptjs(withGoogleMap((props) =>
-    <GoogleMap
-        defaultZoom={14}
-        defaultCenter={{ lat: location.lat, lng: location.lng }}
-        >
-      </GoogleMap>
-    ))
-
-    return <MapComponent
-        googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyBOUHjjOeuaEb8aIp2punWar97hvnyRhDE"
-        loadingElement={<div style={{ height: `100%` }} />}
-        containerElement={<div style={{ height: `100%` }} />}
-        mapElement={<div style={{ height: `100%` }} />}
-    />
+    useEffect(() => {
+        if (!map.current) return; // wait for map to initialize        
+        map.current.on('move', () => {
+            setLng(map.current.getCenter().lng.toFixed(4));
+            setLat(map.current.getCenter().lat.toFixed(4));
+            setZoom(map.current.getZoom().toFixed(2));
+        });       
+    });
+    
+    return (
+        <div>
+            <div ref={mapContainer} className="map-container" />
+        </div>
+    );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,11 @@
-import React, {useEffect, useState} from "react";
+import React from "react";
 import Map from "../components/Map";
-import SideBar from "../components/SideBar";
 import "../styles/home.scss";
 
 export default function Home() { 
     
     return (
         <div className="container">
-            <SideBar />
             <div className="map-container">
                 <Map/>
            </div>
